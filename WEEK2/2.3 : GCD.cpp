f382c01539856f97bdbf53f2148d1df04c5e78ad
Nishi-16-K <76994658+Nishi-16-K@users.noreply.github.com>
2021-05-15 13:52:02 +0530

    Create 2.3 : GCD.cpp

---
+++ WEEK2/2.3 : GCD.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+typedef   long long int  ll;
+
+using namespace std;
+int GCD(int a , int b) {
+	if (a == 0)
+		return b;
+		else
+	return GCD (b%a,a);
+}
+int main() {
+	int a, b;
+	cin >> a >> b;
+	cout << GCD(a, b) << endl;
+}
