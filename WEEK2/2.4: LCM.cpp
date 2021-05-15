08149d5527fc923bc313a8883906d85bd509ed8a
Nishi-16-K <76994658+Nishi-16-K@users.noreply.github.com>
2021-05-15 13:53:38 +0530

    Create 2.4: LCM.cpp

---
+++ WEEK2/2.4: LCM.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+typedef   long long int               ll;
+
+using namespace std;
+ll gcd(ll a, ll b) {
+	while (b) {
+		ll tmp = a % b;
+		a = b;
+		b = tmp;
+	}
+	return a;
+}
+
+ll lcm(ll a, ll b) {
+	return a / (ll)(gcd(a, b))* b;
+}
+int main() {
+	int a, b;
+	cin >> a >> b;
+	cout << lcm(a, b) << endl;
+}
