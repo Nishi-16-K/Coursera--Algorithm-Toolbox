dcd8ffac91ac1d5a8bbfa53e4ec52b11f6590d4d
Nishi-16-K <76994658+Nishi-16-K@users.noreply.github.com>
2021-05-15 14:12:09 +0530

    Create  3.4: Max_Adv_Rev.cpp

---
+++ WEEK 3/3.4: Max_Adv_Rev.cpp
@@ -0,0 +1,32 @@
+#include <algorithm>
+#include <functional>
+#include <iostream>
+#include <iterator>
+#include <numeric>
+#include <vector>
+
+using namespace std;
+
+long long max_product(vector<int> a, vector<int> b) {
+	sort(a.begin(), a.end());
+	sort(b.begin(), b.end());
+
+	vector<long long> c;
+	c.reserve(a.size());
+
+transform(a.begin(), a.end(), b.begin(), back_inserter(c),
+				   multiplies<long long>());
+
+	long long result = accumulate(c.begin(), c.end(), 0ll);
+
+	return result;
+}
+
+int main() {
+	size_t n;
+	cin >> n;
+	vector<int> a(n), b(n);
+	for (size_t i = 0; i < n; i++) { cin >> a[i]; }
+	for (size_t i = 0; i < n; i++) { cin >> b[i]; }
+	cout << max_product(a, b) <<endl;
+}
